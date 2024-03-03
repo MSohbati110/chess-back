1fc8853f3557f4fe6d351b1c4e3939f82979bad4
MSohbati110 <mostafa.sohbati2@gmail.com>
2024-03-03 18:56:47 +0330

    feat: user registration

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
-
+from . import views
 
 urlpatterns = [
   path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+  path('register/', views.Register.as_view(), name="register_new_user"),
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+class Register(APIView):
+  def post(self, request):
+    if User.objects.filter(username=request.data['username']).count() == 0:
+      if User.objects.filter(email=request.data['email']).count() == 0:
+        user = User.objects.create_user(request.data['username'], request.data['email'], request.data['password'])
+        refresh = RefreshToken.for_user(user)
+        return Response(str(refresh.access_token))
+      else:
+        return Response({'type': 'email', 'text': 'Email address already taken'}, status=400)
+    else:
+      return Response({'type': 'username', 'text': 'This username is already in use, please try another one.'}, status=400)
